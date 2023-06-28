09aeb471647b28ac18dc6d47f2facac7fb0f607d
Leo Cozzens <leo@narthexnetworks.com>
2023-06-27 22:56:29 -0500

    Initial commit

---
+++ src/jsnake/Main.java
@@ -0,0 +1,9 @@
+package jsnake;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Test");
+    }
+}
